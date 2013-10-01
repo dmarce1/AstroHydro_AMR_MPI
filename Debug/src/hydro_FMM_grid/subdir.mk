6db878fea460b9d9ce7cfdbfe4c930b15a564da8
Dominic Marcello <dmarce1@lsu.edu>
2013-10-01 10:00:36 -0500

    Debug makefile

---
+++ Debug/src/hydro_FMM_grid/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/hydro_FMM_grid/hydro_FMM_grid.cpp \
+../src/hydro_FMM_grid/moment.cpp \
+../src/hydro_FMM_grid/taylor.cpp 
+
+OBJS += \
+./src/hydro_FMM_grid/hydro_FMM_grid.o \
+./src/hydro_FMM_grid/moment.o \
+./src/hydro_FMM_grid/taylor.o 
+
+CPP_DEPS += \
+./src/hydro_FMM_grid/hydro_FMM_grid.d \
+./src/hydro_FMM_grid/moment.d \
+./src/hydro_FMM_grid/taylor.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/hydro_FMM_grid/%.o: ../src/hydro_FMM_grid/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Intel Intel(R) 64 C++ Compiler '
+	mpic++ -g -I/home/dmarce1/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
